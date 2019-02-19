93e954bc090356929aecfff2123cd09ade23ce87
TatiRaspPi <tiadisu@gmail.com>
2019-02-19 14:13:53 +0000

    Added the elapsed time to work out how long the pi has been held

--- scripts/motion_plus_vibration.py
+++ scripts/motion_plus_vibration.py
@@ -5,22 +5,25 @@ import datetime
 
 from envirophat import motion, leds
 
-def LoggingData():
-	now = time.time()
+def LoggingData(elapsed):
+    print (elapsed)
+    now = time.time()
 
-	r = requests.post("https://mysterious-savannah-98391.herokuapp.com/add_data",
-                  	json={'time': now,
-                        	'value': 1.0})
+    r = requests.post("https://mysterious-savannah-98391.herokuapp.com/add_data",
+                    json={'time': now,
+                            'value': elapsed})
 
-	if r.status_code == requests.codes.ok:
-    		data = r.json()
-    		print("Data OK: ", data)
-	else:
-    		print("error fetching, status is ", r.status_code)
+    if r.status_code == requests.codes.ok:
+            data = r.json()
+            print("Data OK: ", data)
+    else:
+            print("error fetching, status is ", r.status_code)
 
 #time 
 timestamp = datetime.datetime.now().isoformat()
 
+#When the angle(x) is in the negative the motion should be undetected
+#When the angle(x) is in the positive the motion should be detected
 #Vibrations
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -34,23 +37,33 @@ Press Ctrl+C to exit.
 #x, y, z = motion.accelerometer()
 #print(z)
 
+start_time = 0
+last_x = 0
+
 try:
     while True:
         x, y, z = motion.accelerometer()
         print(x)
         time.sleep(1)
-        if x > 0:
+        if x > 0 and last_x < 0:
+            #first time is the package and second is the seconds time
+            start_time = time.time()
             print("Motion Detected")
             leds.on()
             print ("Vibration On")
             GPIO.output(18,GPIO.HIGH)
-	    LoggingData()
-        else:
+        elif x < 0 and last_x > 0:
+            elapsed = time.time() - start_time
             print("Motion UnDetected")
             leds.off()
             print ("Vibration Off")
             GPIO.output(18,GPIO.LOW)
+            LoggingData(elapsed)
+#updates x
 
+        last_x = x
+            
 except KeyboardInterrupt:
     pass
+
 
